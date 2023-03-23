42e265d10a43d1b6e0670f8042835e70a16e567f
DanielHalachev <dani22h09_pl@abv.bg>
2023-03-23 15:56:11 +0200

    feat: Added universal CSS import statements for the common parts on all pages

---
+++ includes/styles.php
@@ -0,0 +1,3 @@
+<link rel="stylesheet" type="text/css" href="./../../css/common.css"/>
+<link rel="stylesheet" type="text/css" href="./../../css/header.css"/>
+<link rel="stylesheet" type="text/css" href="./../../css/footer.css"/>
