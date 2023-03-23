e4d5d7712304ade338f604b32eeccb5edf3f04d1
DanielHalachev <dani22h09_pl@abv.bg>
2023-03-23 15:53:40 +0200

    feat: Added a simple flexible footer

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+<footer>
+  <!-- <div class="footer-container"> -->
+      <div class="footer-col-small">
+        <p>&copy; 2023-<span id="footer-year"></span> EverLearn. All rights reserved.</p>
+      </div>
+      <div class="footer-col-small">
+        <p>Powered by SIMP</p>
+      </div>
+  <!-- </div> -->
+</footer>
