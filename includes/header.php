2b469258c9f77818203ad851584a480101ee2cf1
DanielHalachev <dani22h09_pl@abv.bg>
2023-03-23 15:54:31 +0200

    feat: Added a universal header with navbar

---
+++ includes/header.php
@@ -0,0 +1,15 @@
+<nav role="navigation">
+  <ul>
+    <li><a>График</a></li>
+    <li><a>Такси</a></li>
+    <li><a aria-haspopup="true">Избираеми</a>
+      <ul class="dropdown" aria-label="submenu">
+        <li><a>Записване на избираеми</a></li>
+        <li><a>Предпочитани избираеми</a></li>
+        <li><a>Моите избираеми</a></li>
+      </ul>
+    </li>
+    <li><a>Съобщения</a></li>
+    <li><a>Име</a></li>
+    <li><a>Изход</a></li>
+</nav>
