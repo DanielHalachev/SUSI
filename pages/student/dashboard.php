6afb6c110ed939e138f1559bdfd82aa113dbce0f
DanielHalachev <dani22h09_pl@abv.bg>
2023-03-23 15:49:16 +0200

    feat: Added student dashboard view

---
+++ pages/student/dashboard.php
@@ -0,0 +1,22 @@
+<html>
+  <head>
+    <?php
+    include "./../../includes/styles.php";
+    include "./../../includes/javascript.php"
+    ?>
+  </head>
+  <body>
+    <header>
+      <h1>Logo</h1>
+      <?php
+        include "./../../includes/header.php"
+      ?>
+    </header>
+    <main>
+    </main>
+    <!-- footer -->
+    <?php
+      include "./../../includes/footer.php"
+    ?>
+  </body>
+</html>
