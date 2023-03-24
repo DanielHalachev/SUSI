33010dfd9d380f315ee2eee713b597b80c03d339
DanielHalachev <dani22h09_pl@abv.bg>
2023-03-24 14:43:51 +0200

    feat: Added logo to all pages

--- includes/header.php
+++ includes/header.php
@@ -1,15 +1,19 @@
-<nav role="navigation">
-  <ul>
-    <li><a>График</a></li>
-    <li><a>Такси</a></li>
-    <li><a aria-haspopup="true">Избираеми</a>
-      <ul class="dropdown" aria-label="submenu">
-        <li><a>Записване на избираеми</a></li>
-        <li><a>Предпочитани избираеми</a></li>
-        <li><a>Моите избираеми</a></li>
-      </ul>
-    </li>
-    <li><a>Съобщения</a></li>
-    <li><a>Име</a></li>
-    <li><a>Изход</a></li>
-</nav>
+<header>
+  <img class="logo" src="./../../images/logo.png"/>
+  <nav role="navigation">
+    <ul>
+      <li><a>График</a></li>
+      <li><a>Такси</a></li>
+      <li><a aria-haspopup="true">Избираеми</a>
+        <ul class="dropdown" aria-label="submenu">
+          <li><a>Записване на избираеми</a></li>
+          <li><a>Предпочитани избираеми</a></li>
+          <li><a>Моите избираеми</a></li>
+        </ul>
+      </li>
+      <li><a>Съобщения</a></li>
+      <li><a>Име</a></li>
+      <li><a>Изход</a></li>
+    </ul>
+  </nav>
+</header>
