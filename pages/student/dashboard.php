e9435386629637e8486174dd6feff4747263338f
DanielHalachev <dani22h09_pl@abv.bg>
2023-03-24 18:43:27 +0200

    refactor: Student Dashboard example content now makes more sense

--- pages/student/dashboard.php
+++ pages/student/dashboard.php
@@ -1,5 +1,7 @@
-<html>
+<!DOCTYPE html>
+<html lang="bg">
 <head>
+
 <?php
 include "./../../includes/styles.php";
 include "./../../includes/javascript.php"
@@ -10,7 +12,7 @@ include "./../../includes/javascript.php"
 <?php
 include "./../../includes/header.php"
 ?>
-<main>
+<main id="hello">
   <div id="timetable" class="cluster">
     <h2 class="cluster-title"><a href="#">Разписание на занятията</a></h2>
     <div class="cluster-content">
@@ -89,7 +91,7 @@ include "./../../includes/header.php"
   </div>
   <div class="cluster">
     <h2><a href="#">Такси</a></h2>
-    <div class="content">
+    <div class="cluster-content">
       <table>
         <thead>
           <tr>
@@ -102,69 +104,78 @@ include "./../../includes/header.php"
           <tr>
             <td>Семестриална такса</td>
             <td>450лв.</td>
-            <td><button class="paid">Платена</button></td>
+            <td class="payment-status"><a class="paid">Платена</a></td>
           </tr>
           <tr>
             <td>Exam retake tax</td>
             <td>50.00</td>
-            <td><button href="#" class="unpaid">Плати</button></td>
+            <td class="payment-status"><a href="#" class="unpaid">Плати</a></td>
           </tr>
         </tbody>
       </table>
     </div>
   </div>
   <div class="cluster">
-    <h2><a href="#">Избираеми дисциплини</a></h2>
-    <div class="content">
-      <div class="left-list">
-        <h3>Маркирани</h3>
-        <table>
-          <thead>
-            <tr>
-              <th>Име</th>
-              <th>Маркер</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Introduction to Programming</td>
-              <td><input type="checkbox" name="mark" value="EL01"></td>
-            </tr>
-            <tr>
-              <td>Database Management Systems</td>
-              <td><input type="checkbox" name="mark" value="EL02" checked></td>
-            </tr>
-            <tr>
-              <td>Web Development</td>
-              <td><input type="checkbox" name="mark" value="EL03"></td>
-            </tr>
-            <tr>
-              <td>Artificial Intelligence</td>
-              <td><input type="checkbox" name="mark" value="EL04"></td>
-            </tr>
-            <!-- add more rows as needed -->
-          </tbody>
-        </table>
-      </div>
-      <div class="right-list">
-        <h3>Записани</h3>
-        <table>
-          <thead>
-            <tr>
-              <th>Име</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr><td>Фрактали</td></tr>
-            <tr><td>Увод в статистиката</td></tr>
-          </tbody>
-        </table>
-      </div>
+    <h2><a href="#">Отбелязани избираеми дисциплини</a></h2>
+    <div class="cluster-content">
+      <table>
+        <thead>
+          <tr>
+            <th>Име</th>
+            <th>Записана</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Introduction to Programming</td>
+            <td><input type="checkbox" name="mark" value="EL01"></td>
+          </tr>
+          <tr>
+            <td>Database Management Systems</td>
+            <td><input type="checkbox" name="mark" value="EL02" checked></td>
+          </tr>
+          <tr>
+            <td>Web Development</td>
+            <td><input type="checkbox" name="mark" value="EL03"></td>
+          </tr>
+          <tr>
+            <td>Artificial Intelligence</td>
+            <td><input type="checkbox" name="mark" value="EL04"></td>
+          </tr>
+          <!-- add more rows as needed -->
+        </tbody>
+      </table>
+    </div>
+  </div>
+  <div class="cluster">
+    <h2><a href="#">Записани избираеми дисциплини</a></h2>
+    <div class="cluster-content">
+      <table>
+        <thead>
+          <tr>
+            <th>Дисциплина</th>
+            <th>Преподавател</th>
+            <th>Кредити</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Фрактали</td>
+            <td>Милко Такев</td>
+            <td>8</td>
+          </tr>
+          <tr>
+            <td>Увод в статистиката</td>
+            <td>Мая Желязкова</td>
+            <td>5</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   </div>
   <div class="cluster">
     <h2><a href="#">Съобщения</a></h2>
-    <div class="content">
+    <div class="cluster-content">
       <ul class="message-list">
         <li class="message">
           <a href="#">Добре дошли в курса по Джава</a>
@@ -186,7 +197,7 @@ include "./../../includes/header.php"
     </div>
     <div class="cluster">
       <h2><a href="#">Оценки</a></h2>
-      <div class="content">
+      <div class="cluster-content">
         <table>
           <thead>
             <tr>
