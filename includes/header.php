0a4a17755a236814211f78ffb5ec9183d30e0e06
DanielHalachev <dani22h09_pl@abv.bg>
2023-03-25 19:18:07 +0200

    feat: Added personal info in navbar

--- includes/header.php
+++ includes/header.php
@@ -5,16 +5,28 @@
     <ul>
       <li><a>График</a></li>
       <li><a>Такси</a></li>
-      <li><a aria-haspopup="true">Избираеми</a>
+      <li><a aria-haspopup="true">Избираеми дисциплини</a>
         <ul class="dropdown" aria-label="submenu">
-          <li><a>Записване на избираеми</a></li>
-          <li><a>Предпочитани избираеми</a></li>
-          <li><a>Моите избираеми</a></li>
+          <li><a>Списък избираеми дисциплини</a></li>
+          <li><a>Предпочитани избираеми дисциплини</a></li>
+          <li><a>Моите избираеми дисциплини</a></li>
         </ul>
       </li>
       <li><a>Съобщения</a></li>
-      <li><a>Име</a></li>
-      <li><a>Изход</a></li>
+      <li id="username">
+        <a>62547</a>
+        <ul class="user-dropdown">
+          <br/>
+          <p><small>Даниел Иванов Халачев</small></p>
+          <p><small>Софтуерно инженерство</small></p>
+          <p><small>Курс 3</small></p>
+          <br/>
+          <hr/>
+          <li><a href="#">Настройки</a></li>
+          <li><a href="#">Моите данни</a></li>
+          <li><a href="#">Изход</a></li>
+        </ul>
+      </li>
     </ul>
   </nav>
 </header>
