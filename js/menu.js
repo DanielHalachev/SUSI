a3b985146b187119732531d1f7d0a9a4555a51b4
DanielHalachev <dani22h09_pl@abv.bg>
2023-03-30 16:47:43 +0300

    fix: Added icon for menu button

--- js/menu.js
+++ js/menu.js
@@ -1,16 +1,18 @@
 // make sure the document has been loaded to find the button
 document.addEventListener("DOMContentLoaded", function() {
-  const menuBtn = document.querySelector('.menu-button img');
+  // const menuBtn = document.querySelector('.menu-button img');
+  const menuBtn = document.querySelector('#menu-button');
   const menu = document.querySelector('nav ul');
 
   let menuOpen = false;
   menuBtn.addEventListener('click', () => {
-    console.log("Hello");
     if (!menuOpen) {
       menu.classList.add('show');
+      menuBtn.classList.replace("fa-bars", "fa-xmark");
       menuOpen = true;
     } else {
       menu.classList.remove('show');
+      menuBtn.classList.replace('fa-xmark', 'fa-bars');
       menuOpen = false;
     }
   });
