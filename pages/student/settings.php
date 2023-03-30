f5af492279835358aed58a6a3553829305962bf4
DanielHalachev <dani22h09_pl@abv.bg>
2023-03-30 16:10:18 +0300

    feat: Added a settings page

---
+++ pages/student/settings.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="bg">
+<head>
+
+<?php
+include "./../../includes/styles.php";
+include "./../../includes/javascript.php"
+?>
+<link rel="stylesheet" type="text/css" href="./../../css/dashboard.css"/>
+</head>
+<body>
+<?php
+include "./../../includes/header.php"
+?>
+<main>
+
+</main>
+<!-- footer -->
+<?php
+include "./../../includes/footer.php"
+?>
+</body>
+</html>
