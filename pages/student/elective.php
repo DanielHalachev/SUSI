0b1eccae70c5da0531dd85531c5a2208549d3d7f
DanielHalachev <dani22h09_pl@abv.bg>
2023-05-18 21:26:29 +0300

    refactor: Improved Elective page

--- pages/student/elective.php
+++ pages/student/elective.php
@@ -6,7 +6,106 @@
       include "./../../includes/styles.php";
       include "./../../includes/javascript.php";
     ?>
-  </head>
+<script src="./../../js/popup.js"></script> 
+<script src="./../../js/columns.js"></script> 
+<script src="./../../js/tableFilter.js"></script> 
+<script src="./../../js/tableSort.js"></script> 
+
+<script>
+document.addEventListener('DOMContentLoaded', function() {
+document.getElementById('enrolledFilter').addEventListener('change', function() {
+      var value = this.value;
+      var table = document.querySelector('table');
+      var rows = table.querySelectorAll('tbody tr');
+      
+      for (var i = 0; i < rows.length; i++) {
+        var button = rows[i].querySelector('td button');
+        if (value === 'enrolled' && button.innerHTML!=="Записана") {
+          rows[i].style.display = 'none';
+        } else if (value === 'enrolled-in-queue' && button.innerHTML !== "Записана в опашка") {
+          rows[i].style.display = 'none';
+        } else if (value === 'not-enrolled' && button.innerHTML !== "Записване") {
+          rows[i].style.display = 'none';
+        } else {
+          rows[i].style.display = '';
+        }
+      }
+    });
+    });
+</script>
+<script>
+document.addEventListener('DOMContentLoaded', function() {
+document.getElementById('markingFilter').addEventListener('change', function() {
+      var value = this.value;
+      var table = document.querySelector('table');
+      var rows = table.querySelectorAll('tbody tr');
+      
+      for (var i = 0; i < rows.length; i++) {
+        var checkbox = rows[i].querySelector('input[type="checkbox"]');
+        if (value === 'marked' && !checkbox.checked) {
+          rows[i].style.display = 'none';
+        } else if (value === 'unmarked' && checkbox.checked) {
+          rows[i].style.display = 'none';
+        } else {
+          rows[i].style.display = '';
+        }
+      }
+    });
+    });
+</script>
+<script>
+document.addEventListener('DOMContentLoaded', function() {
+  const buttons = document.querySelectorAll('.neutral, .positive');
+
+  buttons.forEach(function(button) {
+    const originalText = button.innerText;
+
+    button.addEventListener('click', function() {
+      button.classList.toggle('neutral');
+      button.classList.toggle('positive');
+
+      if (button.classList.contains('neutral')) {
+        button.innerText = 'Записана';
+      } else {
+        button.innerText = originalText;
+      }
+    });
+
+    button.addEventListener('mouseover', function() {
+      if (button.classList.contains('neutral')) {
+        button.innerText = 'Отписване';
+      }
+    });
+
+    button.addEventListener('mouseout', function() {
+      if (button.classList.contains('neutral')) {
+        button.innerText = 'Записана';
+      }
+    });
+  });
+});
+</script>
+<script>
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('markingFilter').addEventListener('change', function() {
+      var value = this.value;
+      var table = document.querySelector('table');
+      var rows = table.querySelectorAll('tbody tr');
+      
+      for (var i = 0; i < rows.length; i++) {
+        var checkbox = rows[i].querySelector('td:last-child input[type="checkbox"]');
+        if (value === 'marked' && !checkbox.checked) {
+          rows[i].style.display = 'none';
+        } else if (value === 'unmarked' && checkbox.checked) {
+          rows[i].style.display = 'none';
+        } else {
+          rows[i].style.display = '';
+        }
+      }
+    });
+    });
+</script>
+
   <body>
     <?php
       include "./../../includes/header.php";
@@ -14,31 +113,40 @@
     <main>
       <h1>Избираеми дисциплини</h1>
       <input type="search" placeholder="Потърсете избираема дисциплина"/>
-      <div class="optional-headers">
-          <td><label><input type="checkbox"/>Име</label></td>
-          <td><label><input type="checkbox"/>Преподавател</label></td>
-          <td><label><input type="checkbox"/>Кредити</label></td>
-          <td><label><input type="checkbox"/>Оценка</label></td>
-          <td><label><input type="checkbox"/>Препоръчителен семестър</label></td>
-          <td><label><input type="checkbox"/>Рейтинг</label></td>
-          <td><label><input type="checkbox"/>Маркиране</label></td>
-          <td><label><input type="checkbox"/>Записване</label></td>
+      <div class="filters">
+          <label><input type="checkbox" checked disabled data-column="1"/>Име</label>
+          <label><input type="checkbox" checked data-column="2"/>Преподавател</label>
+          <label><input type="checkbox" checked data-column="3"/>Кредити</label>
+          <label><input type="checkbox" checked data-column="4"/>Оценка</label>
+          <label><input type="checkbox" checked data-column="5"/>Препоръчителен семестър</label>
+          <label><input type="checkbox" checked data-column="6"/>Рейтинг</label>
+          <label><input type="checkbox" checked data-column="7"/>Маркиране</label>
+        <select id="markingFilter">
+          <option default value="all">Маркирани и немаркирани</option>
+          <option value="marked">Маркирани</option>
+          <option value="unmarked">Немаркирани</option>
+        </select>
+        <select id="enrolledFilter">
+          <option default value="all">Всички</option>
+          <option value="enrolled">Записани</option>
+          <option value="enrolled-in-queue">Записани в опашка</option>
+          <option value="not-enrolled">Незаписани</option>
+        </select>
+    <span class="export fa fa-file-csv"></span>
+    <span class="export fa fa-file-pdf"></span>
       </div>
-      <br/>
-      <hr/>
-      <br/>
       <div>
         <table>
         <thead>
         <tr>
-          <th>Име <span class="fa fa-filter"></span></th>
-          <th>Преподавател <span class="fa fa-filter"></span></th>
-          <th>Кредити <span class="fa fa-filter"></span></th>
-          <th>Оценка <span class="fa fa-filter"></span></th>
-          <th>Препоръчителен семестър <span class="fa fa-filter"></span></th>
-          <th>Рейтинг <span class="fa fa-filter"></span></th>
-          <th>Маркиране <span class="fa fa-filter"></span></th>
-          <th>Записване <span class="fa fa-filter"></span></th>
+          <th data-column="1" onclick="sortTable(this)">Име <span class="fa fa-sort"></span></th>
+          <th data-column="2" onclick="sortTable(this)">Преподавател <span class="fa fa-sort"></span></th>
+          <th data-column="3" onclick="sortTable(this)">Кредити <span class="fa fa-sort"></span></th>
+          <th data-column="4" onclick="sortTable(this)">Оценка <span class="fa fa-sort"></span></th>
+          <th data-column="5" onclick="sortTable(this)">Препоръчителен семестър <span class="fa fa-sort"></span></th>
+          <th data-column="6" onclick="sortTable(this)">Рейтинг <span class="fa fa-sort"></span></th>
+          <th data-column="7" onclick="sortTable(this)">Маркиране <span class="fa fa-sort"></span></th>
+          <th data-column="8" onclick="sortTable(this)">Записване <span class="fa fa-sort"></span></th>
         </tr>
         </thead>
         <tbody>
@@ -70,7 +178,7 @@
   <td>2</td>
   <td>4.5</td>
   <td><input type="checkbox"/></td>
-  <td><button class="negative">Отписване</button></td>
+  <td><button class="neutral">Записана в опашка</button></td>
 </tr>
 <tr>
   <td>Криптография</td>
@@ -80,7 +188,7 @@
   <td>4</td>
   <td>4.8</td>
   <td><input type="checkbox"/></td>
-  <td><button class="neutral">Записване в опашка</button></td>
+  <td><button class="positive">Записване в опашка</button></td>
 </tr>
 <tr>
   <td>Интернет програмиране</td>
@@ -100,7 +208,7 @@
   <td>2</td>
   <td>4.5</td>
   <td><input type="checkbox"/></td>
-  <td><button class="neutral">Записване в опашка</button></td>
+  <td><button class="positive">Записване в опашка</button></td>
 </tr>
        <tr>
   <td>Увод в програмирането</td>
@@ -120,7 +228,7 @@
   <td>1</td>
   <td>4.3</td>
   <td><input type="checkbox"/></td>
-  <td><button class="negative">Отписване</button></td>
+  <td><button class="neutral">Записана</button></td>
 </tr>
 <tr>
   <td>Основи на базите данни</td>
