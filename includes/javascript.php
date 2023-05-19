09957bba1a397daded2674e9dcd3e09295c3f587
DanielHalachev <dani22h09_pl@abv.bg>
2023-05-19 20:40:55 +0300

    feat: Added light-dark mode option

--- includes/javascript.php
+++ includes/javascript.php
@@ -1,3 +1,4 @@
 <script src="./../../js/footer.js" type="text/javascript"></script>
 <script src="./../../js/menu.js" type="text/javascript"></script>
 <script src="https://kit.fontawesome.com/5c9473fc67.js" crossorigin="anonymous"></script>
+<script src="./../../js/theme.js" type="text/javascript"></script>
