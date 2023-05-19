2acafba69f168d7047fbbfe93dfbdf2db5b6c720
DanielHalachev <dani22h09_pl@abv.bg>
2023-05-19 20:41:52 +0300

    refact: Minor refactor of settings page to make it compliant with the rest

--- pages/student/settings.php
+++ pages/student/settings.php
@@ -8,6 +8,17 @@ include "./../../includes/javascript.php"
 ?>
 <link rel="stylesheet" type="text/css" href="./../../css/student/settings.css"/>
 <script src="./../../js/tabs.js"></script>
+<script>
+window.addEventListener('load', function() {
+  const mainCheckbox = document.querySelector('.main-option input');
+  const subOptions = document.querySelector('.sub-options');
+
+  mainCheckbox.addEventListener('change', () => {
+    subOptions.classList.toggle('visible', mainCheckbox.checked);
+    subOptions.querySelectorAll('input[type="checkbox"]').forEach(function (checkbox){checkbox.disabled = !checkbox.disabled});
+  });
+});
+</script>
 </head>
 <body>
 <?php
@@ -17,10 +28,11 @@ include "./../../includes/header.php"
   <div class="personal-info">
     <div class="image-settings">
       <img src="./../../images/profile-pic.jpg">
-        <div>
-      <a href="#"><span class="fa fa-pen-to-square"></span</a>
-      <a href="#"><span class="fa fa-trash"></span></a>
-        </div>
+      <div>
+        <button><span class="fa fa-pen-to-square"></span> Редактиране</button>
+        <button><span class="fa fa-trash"></span> Премахване</button>
+        <button><span class="fa fa-key"></span> Смяна на парола</button>
+      </div>
     </div>
     <table>
       <tbody>
@@ -39,7 +51,7 @@ include "./../../includes/header.php"
         <tr>
           <td>Факултетен номер</td>
           <td>62547</td>
-</tr>
+        </tr>
         <tr>
           <td>Курс</td>
           <td>3</td>
@@ -65,11 +77,15 @@ include "./../../includes/header.php"
   </div>
   <div class="tabs">
     <div class="tabline">
-      <button class="tablinks" onclick="openTab(event, 'taxes')">Такси</button>
-      <button class="tablinks" onclick="openTab(event, 'health-insurance')">Здравно осигуряване</button>
-      <button class="tablinks" onclick="openTab(event, 'logins')">Логвания</button>
+      <button class="neutral tablinks" onclick="openTab(event, 'taxes')">Такси</button>
+      <button class="neutral tablinks" onclick="openTab(event, 'health-insurance')">Здравно осигуряване</button>
+      <button class="neutral tablinks" onclick="openTab(event, 'logins')">Логвания</button>
+      <button class="neutral tablinks" onclick="openTab(event, 'card-settings')">Настройки на началната страница</button>
+      <button class="neutral tablinks" onclick="openTab(event, 'email-settings')">Настройки на известията</button>
+      <button class="neutral tablinks" onclick="openTab(event, 'theme-settings')">Tема</button>
     </div> 
     <div id="taxes" class="tabcontent">
+      <h2>Такси</h2>
       <table>
         <thead>
           <tr>
@@ -82,178 +98,203 @@ include "./../../includes/header.php"
           <tr>
             <td>Семестриална такса</td>
             <td>450.00</td>
-            <td class="payment-status"><a class="positive">Платена</a></td>
+            <td class="payment-status"><a class="neutral">Платена</a></td>
           </tr>
-          <tr>
+          <tr id="hidden-tax" style="display: none;">
             <td>Дискретни структури 1</td>
             <td>50.00</td>
-            <td class="payment-status"><a href="#" class="negative">Плати</a></td>
+            <td class="payment-status"><a href="#" class="positive">Плати</a></td>
           </tr>
         </tbody>
       </table>
       <br/>
       <hr/>
       <br/>
-      <form>
+      <div>
         <select>
           <option selected disabled>Изберете предмет</option>
           <option>Дискретни структури 1</option>
         </select>
-        <input type="submit" class="button" value="Генериране на такса"/>
-      </form>
-          
+        <button class="neutral" onclick="document.getElementById('hidden-tax').style.display='table-row';">Генериране на такса</button>
+      </div>
+
     </div>
     <div id="health-insurance" class="tabcontent">
+      <h2>Здравно осигуряване</h2>
       <table>
-  <thead>
-    <tr>
-      <th>Година</th>
-      <th>Януари</th>
-      <th>Февруари</th>
-      <th>Март</th>
-      <th>Април</th>
-      <th>Май</th>
-      <th>Юни</th>
-      <th>Юли</th>
-      <th>Август</th>
-      <th>Септември</th>
-      <th>Октомври</th>
-      <th>Ноември</th>
-      <th>Декември</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th>2020</th>
-      <td class="unpaid"></td>
-      <td class="unpaid"></td>
-      <td class="unpaid"></td>
-      <td class="unpaid"></td>
-      <td class="unpaid"></td>
-      <td class="unpaid"></td>
-      <td class="unpaid"></td>
-      <td class="unpaid"></td>
-      <td class="unpaid"></td>
-      <td class="paid"></td>
-      <td class="paid"></td>
-      <td class="paid"></td>
-    </tr>
-    <tr>
-      <th>2021</th>
-      <td class="paid"></td>
-      <td class="paid"></td>
-      <td class="paid"></td>
-      <td class="paid"></td>
-      <td class="paid"></td>
-      <td class="paid"></td>
-      <td class="paid"></td>
-      <td class="paid"></td>
-      <td class="paid"></td>
-      <td class="paid"></td>
-      <td class="paid"></td>
-      <td class="paid"></td>
-    </tr>
-    <tr>
-      <th>2022</th>
-      <td class="paid"></td>
-      <td class="paid"></td>
-      <td class="paid"></td>
-      <td class="paid"></td>
-      <td class="paid"></td>
-      <td class="paid"></td>
-      <td class="paid"></td>
-      <td class="paid"></td>
-      <td class="paid"></td>
-      <td class="paid"></td>
-      <td class="paid"></td>
-      <td class="paid"></td>
-    </tr>
-    <tr>
-      <th>2023</th>
-      <td class="paid"></td>
-      <td class="paid"></td>
-      <td class="paid"></td>
-      <td class="unpaid"></td>
-      <td class="unpaid"></td>
-      <td class="unpaid"></td>
-      <td class="unpaid"></td>
-      <td class="unpaid"></td>
-      <td class="unpaid"></td>
-      <td class="unpaid"></td>
-      <td class="unpaid"></td>
-      <td class="unpaid"></td>
-    </tr>
-  </tbody>
-</table>
+        <thead>
+          <tr>
+            <th>Година</th>
+            <th>Януари</th>
+            <th>Февруари</th>
+            <th>Март</th>
+            <th>Април</th>
+            <th>Май</th>
+            <th>Юни</th>
+            <th>Юли</th>
+            <th>Август</th>
+            <th>Септември</th>
+            <th>Октомври</th>
+            <th>Ноември</th>
+            <th>Декември</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th>2020</th>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td><span class="fa fa-check"></span></td>
+            <td><span class="fa fa-check"></span></td>
+            <td><span class="fa fa-check"></span></td>
+          </tr>
+          <tr>
+            <th>2021</th>
+            <td><span class="fa fa-check"></span></td>
+            <td><span class="fa fa-check"></span></td>
+            <td><span class="fa fa-check"></span></td>
+            <td><span class="fa fa-check"></span></td>
+            <td><span class="fa fa-check"></span></td>
+            <td><span class="fa fa-check"></span></td>
+            <td><span class="fa fa-check"></span></td>
+            <td><span class="fa fa-check"></span></td>
+            <td><span class="fa fa-check"></span></td>
+            <td><span class="fa fa-check"></span></td>
+            <td><span class="fa fa-check"></span></td>
+            <td><span class="fa fa-check"></span></td>
+          </tr>
+          <tr>
+            <th>2022</th>
+            <td><span class="fa fa-check"></span></td>
+            <td><span class="fa fa-check"></span></td>
+            <td><span class="fa fa-check"></span></td>
+            <td><span class="fa fa-check"></span></td>
+            <td><span class="fa fa-check"></span></td>
+            <td><span class="fa fa-check"></span></td>
+            <td><span class="fa fa-check"></span></td>
+            <td><span class="fa fa-check"></span></td>
+            <td><span class="fa fa-check"></span></td>
+            <td><span class="fa fa-check"></span></td>
+            <td><span class="fa fa-check"></span></td>
+            <td><span class="fa fa-check"></span></td>
+          </tr>
+          <tr>
+            <th>2023</th>
+            <td><span class="fa fa-check"></span></td>
+            <td><span class="fa fa-check"></span></td>
+            <td><span class="fa fa-check"></span></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
     </div>
     <div id="logins" class="tabcontent">
+      <h2>Логвания</h2>
       <table>
-  <thead>
-    <tr>
-      <th>Дата и час</th>
-      <th>Устройство</th>
-      <th>IP адрес</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>27/01/2023 14:05:23</td>
-      <td>Windows 10 / Chrome 97.0.4692.99</td>
-      <td>192.168.1.1</td>
-    </tr>
-    <tr>
-      <td>25/01/2023 08:47:19</td>
-      <td>Mac OS X / Safari 15.1</td>
-      <td>172.16.0.2</td>
-    </tr>
-    <tr>
-      <td>23/01/2023 17:31:11</td>
-      <td>Windows 11 / Firefox 95.0</td>
-      <td>10.0.0.1</td>
-    </tr>
-    <tr>
-      <td>21/01/2023 22:09:45</td>
-      <td>iOS 15 / Safari 15.1</td>
-      <td>192.168.0.10</td>
-    </tr>
-    <tr>
-      <td>19/01/2023 11:15:33</td>
-      <td>Android 12 / Chrome 96.0.4664.93</td>
-      <td>192.168.1.100</td>
-    </tr>
-    <tr>
-      <td>17/01/2023 16:22:01</td>
-      <td>Windows 10 / Edge 98.0.1108.56</td>
-      <td>192.168.1.50</td>
-    </tr>
-    <tr>
-      <td>15/01/2023 09:43:10</td>
-      <td>Linux / Firefox 95.0.1</td>
-      <td>172.16.0.100</td>
-    </tr>
-    <tr>
-      <td>13/01/2023 14:58:21</td>
-      <td>Mac OS X / Chrome 97.0.4692.99</td>
-      <td>192.168.1.2</td>
-    </tr>
-    <tr>
-      <td>11/01/2023 18:30:05</td>
-      <td>iOS 14 / Safari 14.1</td>
-      <td>10.0.0.2</td>
-    </tr>
-    <tr>
-      <td>09/01/2023 21:05:57</td>
-      <td>Windows 11 / Edge 98.0.1108.56</td>
-      <td>192.168.1.20</td>
-    </tr>
-    <tr>
-      <td>07/01/2023 12:18:13</td>
-      <td>Android 11 / Chrome 96.0.4664.93</td>
-      <td>192.168.0.2</td>
-    </tr>
-  </tbody>
-  </table>
-  </div>
+        <thead>
+          <tr>
+            <th>Дата и час</th>
+            <th>Устройство</th>
+            <th>IP адрес</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>27/01/2023 14:05:23</td>
+            <td>Windows 10 / Chrome 97.0.4692.99</td>
+            <td>192.168.1.1</td>
+          </tr>
+          <tr>
+            <td>25/01/2023 08:47:19</td>
+            <td>Mac OS X / Safari 15.1</td>
+            <td>172.16.0.2</td>
+          </tr>
+          <tr>
+            <td>23/01/2023 17:31:11</td>
+            <td>Windows 11 / Firefox 95.0</td>
+            <td>10.0.0.1</td>
+          </tr>
+          <tr>
+            <td>21/01/2023 22:09:45</td>
+            <td>iOS 15 / Safari 15.1</td>
+            <td>192.168.0.10</td>
+          </tr>
+          <tr>
+            <td>19/01/2023 11:15:33</td>
+            <td>Android 12 / Chrome 96.0.4664.93</td>
+            <td>192.168.1.100</td>
+          </tr>
+          <tr>
+            <td>17/01/2023 16:22:01</td>
+            <td>Windows 10 / Edge 98.0.1108.56</td>
+            <td>192.168.1.50</td>
+          </tr>
+          <tr>
+            <td>15/01/2023 09:43:10</td>
+            <td>Linux / Firefox 95.0.1</td>
+            <td>172.16.0.100</td>
+          </tr>
+          <tr>
+            <td>13/01/2023 14:58:21</td>
+            <td>Mac OS X / Chrome 97.0.4692.99</td>
+            <td>192.168.1.2</td>
+          </tr>
+          <tr>
+            <td>11/01/2023 18:30:05</td>
+            <td>iOS 14 / Safari 14.1</td>
+            <td>10.0.0.2</td>
+          </tr>
+          <tr>
+            <td>09/01/2023 21:05:57</td>
+            <td>Windows 11 / Edge 98.0.1108.56</td>
+            <td>192.168.1.20</td>
+          </tr>
+          <tr>
+            <td>07/01/2023 12:18:13</td>
+            <td>Android 11 / Chrome 96.0.4664.93</td>
+            <td>192.168.0.2</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div id="card-settings" class="tabcontent">
+      <h2>Настройки на началната страница</h2>
+      <label class="main-option"><input type="checkbox" checked />Статични карти</label>
+      <div class="sub-options visible">
+        <label><input type="checkbox" checked/>Седмично разписание</label> 
+        <label><input type="checkbox" checked/>Такси</label> 
+        <label><input type="checkbox" checked/>Маркирани дисциплини</label>
+        <label><input type="checkbox" checked/>Записани избираеми дисциплини</label>
+        <label><input type="checkbox" checked/>Съобщения</label>
+        <label><input type="checkbox" checked/>Оценки от последната активна сесия</label>
+      </div>
+    </div>
+    <div id="email-settings" class="tabcontent">
+      <h2>Настройки на известията</h2> 
+      <p>Получаване на имейл при:</p>
+      <label><input type="checkbox" checked/>промяна в графика</label> 
+      <label><input type="checkbox" checked/>нова такса</label> 
+      <label><input type="checkbox" checked/>промяна на позиция в опашката на избираема дисциплина</label> 
+    </div>
+    <div id="theme-settings" class="tabcontent">
+      <h2>Тема</h2>
+      <label><input type="checkbox" onchange="toggleTheme()"/>Тъмна тема</label> 
+    </div>
   </div>
 </main>
 <!-- footer -->
