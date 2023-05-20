6dabb1eadba4f710c5dd8c1d647ae6d6e6d328b6
Stefan Velev <sdvelev@gmail.com>
2023-05-20 10:15:55 +0300

    feat: Added initial version of page for messaging

---
+++ pages/teacher/messages.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="bg">
+<head>
+
+    <?php
+    include "./../../includes/styles.php";
+    include "./../../includes/javascript.php"
+    ?>
+    <link rel="stylesheet" type="text/css" href="../../css/teacher/messages.css"/>
+</head>
+<body>
+<?php
+include "./../../includes/teacher/header.php"
+?>
+
+
+<main>
+
+
+</main>
+
+
+<!-- footer -->
+<?php
+include "./../../includes/footer.php"
+?>
+</body>
+</html>
