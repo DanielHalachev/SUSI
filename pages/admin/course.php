40d58ec5f6739adaade3178c8da05c26e059ca67
David-Petrov <david.petrov2000@gmail.com>
2023-05-20 20:42:35 +0300

    na pavel formi4kata

--- pages/admin/course.php
+++ pages/admin/course.php
@@ -1,107 +1,116 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-    <link rel="stylesheet" type="text/css" href="./../../css/student/grades.css"/>
-    <?php
-    include "./../../includes/styles.php";
-    include "./../../includes/javascript.php";
-    $mode = $_REQUEST['mode'] ?? 'new';
-    $title = match ($mode) {
-        "new" => "Създаване на нов курс",
-        "edit" => "Редактиране на курс",
-        "copy" => "Копиране на курс",
-    };
-    ?>
-    <title><?=$title?></title>
+  <link rel="stylesheet" type="text/css" href="./../../css/admin/course.css" />
+  <?php
+  include "./../../includes/styles.php";
+  include "./../../includes/javascript.php";
+  $mode = $_REQUEST['mode'] ?? 'new';
+  $title = match ($mode) {
+    "new" => "Създаване на нов курс",
+    "edit" => "Редактиране на курс",
+    "copy" => "Копиране на курс",
+  };
+  ?>
+  <title><?= $title ?></title>
 </head>
+
 <body>
-<?php
-include "./../../includes/admin/header.php";
-?>
-<main>
-    <h1><?=$title?></h1>
-
+  <?php
+  include "./../../includes/admin/header.php";
+  ?>
+  <h1><?= $title ?></h1>
+  <main>
     <form>
-        <label for="name">
-            Име
-            <input required type="text" name="name" id="name"/>
-        </label>
+      <div class="grid-2x3">
+        <div class="label-input-pair">
+          <span>Име:</span>
+          <input required type="text" name="name" id="name" class="input-field" />
+        </div>
 
+        <div class="label-input-pair">
+          <span>Академична година:</span>
+          <select name="year" class="input-field">
+            <option value="23">2023/24</option>
+            <option value="22" selected>2022/23</option>
+          </select>
+        </div>
 
-        <label>
-            Академична година
-            <select name="year">
-                <option value="23">2023/24</option>
-                <option value="22" selected>2022/23</option>
-            </select>
-        </label>
+        <div class="label-input-pair">
+          <span>Семестър:</span>
+          <select name="semester" class="input-field">
+            <option value="winter">Зимен</option>
+            <option value="summer" selected>Летен</option>
+          </select>
+        </div>
 
+        <div class="label-input-pair">
+          <span>Титуляр:</span>
+          <select name="teacher" class="input-field">
+            <option value="" disabled selected>Изберете титуляр</option>
+            <?php
+            $teachers = [
+              'Ангел Дичев',
+              'Милен Чечев',
+              'Олег Константинов'
+            ];
+            foreach ($teachers as $teacher) :
+            ?>
+              <option value="<?= $teacher ?>"><?= $teacher ?></option>
+            <?php endforeach; ?>
+          </select>
+        </div>
 
-        <label>
-            Семестър
-            <select name="semester">
-                <option value="winter">Зимен</option>
-                <option value="summer" selected>Летен</option>
-            </select>
-        </label>
+        <div class="label-input-pair">
+          <span>ECTS:</span>
+          <input type="number" name="ects" class="input-field" />
+        </div>
 
-        <label>
-            Титуляр
-            <select name="teacher">
-                <option value="" disabled selected>Изберете титуляр</option>
-                <?php
-                $teachers = [
-                    'Ангел Дичев',
-                    'Милен Чечев',
-                    'Олег Константинов'
-                ];
-                foreach ($teachers as $teacher):
-                ?>
-                    <option value="1"><?=$teacher?></option>
-                <?php endforeach;?>
-            </select>
-        </label>
+        <div class="label-input-pair">
+          <span>Начин на оценяване:</span>
+          <select class="scoring-options">
+              <option value="" disabled selected>Изберете начин на оценяване</option>
+              <option value="1">Изпит</option>
+              <option value="2">Текуща оценка</option>
+              <option value="3">Изпит + текуща оценка</option>
+          </select>
+        </div>
+      </div>
 
-        <fieldset>
-            <legend>Хорариум (общо в часове):</legend>
-            <label>
-                Лекции
-                <input type="number" name="lectures"/>
-            </label>
-            <label>
-                Семинарни упражнения
-                <input type="number" name="seminar"/>
-            </label>
-            <label>
-                Практически упражнения
-                <input type="number" name="practical"/>
-            </label>
+      <div class="fieldsets-wrapper">
+        <fieldset class="last-fieldset">
+          <legend>Хорариум (общо в часове):</legend>
+          <div class="label-input-pair">
+            <span>Лекции:</span>
+            <input type="number" name="lectures" class="input-field" />
+          </div>
+          <div class="label-input-pair">
+            <span>Семинарни упражнения:</span>
+            <input type="number" name="seminar" class="input-field" />
+          </div>
+          <div class="label-input-pair">
+            <span>Практически упражнения:</span>
+            <input type="number" name="practical" class="input-field" />
+          </div>
         </fieldset>
+      </div>
 
-        <label>
-            Конспект
-            <input type="file" name="conspect"/>
-        </label>
-
-        <label>
-            Начин на оценяване
-            <select>
-                <option value="1">Изпит</option>
-                <option value="2">Текуща оценка</option>
-                <option value="3">Изпит + текуща оценка</option>
-            </select>
-        </label>
-
-        <label>
-            ECTS
-            <input type="number" name="ects"/>
-        </label>
+      <div class="last-row">
+        <div class="label-input-pair">
+          <span>Конспект:</span>
+          <input type="file" name="conspect" class="input-field" />
+        </div>
 
         <button type="submit" class="positive">Създай</button>
+      </div>
     </form>
-</main>
-<?php
-include "./../../includes/footer.php";
-?>
+  </main>
+  <?php
+  include "./../../includes/footer.php";
+  ?>
 </body>
+
 </html>
+
+<!-- :vim: shiftwidth=2: -->
