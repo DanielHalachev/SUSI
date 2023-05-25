000f83c8267eb101d93124fcb0efd3aa2e91822e
DanielHalachev <dani22h09_pl@abv.bg>
2023-05-25 16:34:12 +0300

    feat: Added admin settings page

--- includes/admin/header.php
+++ includes/admin/header.php
@@ -19,9 +19,50 @@
         </li>
         <?php endforeach;?>
         <li id="username">
-            <span class="fa fa-user"></span>
-            <a>Админ Админов</a>
+          <span class="fa fa-user"></span>
+          Админ Админов
+          <ul class="user-dropdown">
+            <div class="user-info">
+              <img class="profile-pic" src="./../../images/profile-pic-admin.jpg"/>
+              <div>
+                <p><small>Админ Админов</small></p>
+                <p><small>Администратор на данни</small></p>
+              </div>  
+            </div>
+            <hr/>
+            <a href="./settings.php?tab=health-insurance">
+              <li>
+                <span class="fa fa-heart-pulse"></span>
+                Здравно осигуряване
+              </li>
+            </a>
+            <a href="./settings.php?tab=logins">
+              <li>
+                <span class="fa fa-right-to-bracket"></span>
+                Логвания
+              </li>
+            </a>
+            <a href="./settings.php?tab=card-settings">
+              <li>
+                <span class="fa fa-home"></span>
+                Настройки на началната страница
+              </li>
+            </a>
+            <a href="./settings.php?tab=theme-settings">
+              <li>
+                <span class="fa fa-circle-half-stroke"></span>
+                Тема
+              </li>
+            </a>
+            <a href="./../login/login.php">
+              <li>
+                <span class="fa fa-power-off"></span>
+                Изход
+              </li>
+            </a>
+          </ul>
         </li>
+
     </ul>
     </nav>
 </header>
